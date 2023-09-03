2a51f9db60fff4aaf8e677ed4423925d21626856
987456987 <donufrak112697@gmail.com>
2023-09-02 21:09:01 -0700

    userInput profile picture

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,7 +1,13 @@
+import { useContext } from "react"
+import { DataContext } from "../utility/DataContext"
+
 const UserInput = () => {
+    const { userData } = useContext(DataContext)
+    const imagePath = `${userData.currentUser.image.webp.substring(1)}`;
+
     return (
         <div id="user-comment-input">
-            <img src="${currentUser.image.webp}" className="user-img" />
+            <img src={imagePath} className="user-img" />
             <textarea id="user-comment-textarea" placeholder="Add a comment..." ></textarea>
             <button className="submit" id="user-comment-input-submit">Send</button>
         </div>
